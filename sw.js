7469bb1ee572fde189fb7c869cf95d4ac680c53b
kenjo-stack <kenjo.dizayee@gmail.com>
2026-09-25 11:45:28 +0100

    Add preferred rest timer and optional routine editing

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* Atomic app-shell cache. New releases wait for the user's Update action. */
-const CACHE='forge50-v2.1.0-guides-readable';
+const CACHE='forge50-v2.2.0-rest-rotation';
 const FILES=['./','./index.html','./style.css','./v2.css','./readability-v21.css','./exercise-guides.css','./exercise-guides.js','./data.js','./js/store.js','./js/timer.js','./js/app.js','./manifest.json','./assets/icon-192.png','./assets/icon-512.png'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(FILES))));
 self.addEventListener('activate',event=>event.waitUntil((async()=>{for(const key of await caches.keys())if(key.startsWith('forge50-')&&key!==CACHE)await caches.delete(key);await self.clients.claim();})()));
